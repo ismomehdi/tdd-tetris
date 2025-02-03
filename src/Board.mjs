f219bafdf1c622ae8eecbf0f50d8140cc8fbacc2
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-02-03 13:39:54 +0200

    Use this.board for constructing the board

--- src/Board.mjs
+++ src/Board.mjs
@@ -5,12 +5,16 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.board = Array.from({ length: height }, () => Array(width).fill(0));
   }
 
   toString() {
     let string = ''
     for (let i = 0; i < this.height; i++) {
-      string += '.'.repeat(this.width) + '\n';
+      for (let j = 0; j < this.board.length; j++) {
+        this.board[i][j] === 0 ? string += '.' : 'X'
+      }
+      string += '\n'
     };
     return string
   }
