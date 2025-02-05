44629f302db574bb97ceca1108d9f0c7dfa90ea1
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-02-05 20:02:39 +0200

    Remove redundant stuff

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -55,38 +55,3 @@ export class Tetromino {
     return new RotatingShape(this.#getShape()).rotateLeft();
   }
 }
-
-export class Tetromino2 {
-  #currentOrientation;
-  #orientations;
-
-  constructor(currentOrientation, orientations) {
-    this.#currentOrientation = (currentOrientation + orientations.length) % orientations.length;
-    this.#orientations = orientations;
-  }
-
-  static T_SHAPE = Tetromino2.fromString(
-    0,
-    4,
-    `.T.
-     TTT
-     ...`
-  );
-
-  static fromString(currentOrientation, orientationCount, initialShape) {
-    const shape = RotatingShape.fromString(initialShape);
-    const orientations = [shape, shape.rotateRight(), shape.rotateRight().rotateRight(), shape.rotateLeft()].slice(
-      0,
-      orientationCount
-    );
-    return new Tetromino(currentOrientation, orientations);
-  }
-
-  #shape() {
-    return this.#orientations[this.#currentOrientation];
-  }
-
-  toString() {
-    return new RotatingShape(this.#shape).toString();
-  }
-}
