6d5768cb3eeb19b988731d448f44f315bf1393c3
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-02-17 21:57:41 +0200

    Add test for moving a falling tetromino right

--- test/FallingTetrominoes.test.mjs
+++ test/FallingTetrominoes.test.mjs
@@ -71,4 +71,18 @@ describe("Falling tetrominoes", () => {
        ..........`
     );
   })
+
+  test.skip('a falling tetromino can be moved right', () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveRight();
+
+    expect(board.toString()).to.equalShape(
+      `.....T....
+       ....TTT...
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  })
 });
