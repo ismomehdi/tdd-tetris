bd0945155ccc967af09d2bdd1b9520e4da0a1944
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-02-18 15:02:24 +0200

    Add orientations following the Arika Rotation System

---
+++ src/orientations.mjs
@@ -0,0 +1,38 @@
+// orientations following the Arika Rotation System
+
+const T_SHAPE = [
+  `....
+   TTT.
+   .T..
+   ....`,
+  `.T..
+   TT..
+   .T..
+   ....`,
+  `....
+   .T..
+   TTT.
+   ....`,
+  `.T..
+   .TT.
+   .T..
+   ....`,
+];
+
+const O_SHAPE = [
+  `....
+   .OO.
+   .OO.
+   ....`,
+];
+
+const I_SHAPE = [
+  `....
+   IIII
+   ....
+   ....`,
+  `..I.
+   ..I.
+   ..I.
+   ..I.`,
+];
