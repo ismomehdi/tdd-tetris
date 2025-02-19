84dd58b392c3bf2062524387114896611bff0e28
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-02-19 12:13:48 +0200

    Add tests for ShuffleBag to verify unpredictable order of values

--- test/ShuffleBag.test.mjs
+++ test/ShuffleBag.test.mjs
@@ -1,5 +1,5 @@
 import { ShuffleBag } from "../src/Shufflebag.mjs";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 
 describe("The shuffle bag", () => {
   test("all values are returned before repeating", () => {
@@ -9,5 +9,27 @@ describe("The shuffle bag", () => {
       values.push(bag.next());
     }
     expect(values).to.have.members([1, 2, 3, 4]);
+  });
+
+  test("values are returned in unpredictable order (based on Math.random)", () => {
+    let mock = vi.fn(() => 0.1);
+    Math.random = mock;
+
+    let bag = new ShuffleBag([1, 2, 3, 4]);
+    let values = [];
+    for (let i = 0; i < 4; i++) {
+      values.push(bag.next());
+    }
+    expect(values).toStrictEqual([2, 3, 4, 1]);
+
+    mock = vi.fn(() => 0.9);
+    Math.random = mock;
+
+    bag = new ShuffleBag([1, 2, 3, 4]);
+    values = [];
+    for (let i = 0; i < 4; i++) {
+      values.push(bag.next());
+    }
+    expect(values).toStrictEqual([1, 2, 3, 4]);
   });
 });
